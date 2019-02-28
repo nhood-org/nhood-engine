638ddc481d96c767ddc10f23d8dec408a2a7d09a
krzysztofreczek <reczek.krzysztof@gmail.com>
2019-02-28 21:34:42 +0100

    NH045 - Add e2e test module

---
+++ nhood-engine-core-test/src/test/java/com/h8/nh/nhoodengine/core/DataFinderAbstractTest.java
@@ -0,0 +1,17 @@
+package com.h8.nh.nhoodengine.core;
+
+
+import org.junit.jupiter.api.Test;
+
+public abstract class DataFinderAbstractTest {
+
+    @Test
+    public void shoudlThrowAnExceptionWhenMetadataVectorIsNull() {
+
+    }
+
+    @Test
+    public void shoudlThrowAnExceptionWhenMetadataVectorSizeDoesNotMatch() {
+
+    }
+}
