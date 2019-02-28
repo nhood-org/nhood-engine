e3e7b7ea2ac4e2c7bb704c2a23265922343f52d3
krzysztofreczek <reczek.krzysztof@gmail.com>
2019-02-28 22:21:44 +0100

    NH045 - Add abstract test context signatures

--- nhood-engine-core-test/src/test/java/com/h8/nh/nhoodengine/core/DataFinderAbstractTest.java
+++ nhood-engine-core-test/src/test/java/com/h8/nh/nhoodengine/core/DataFinderAbstractTest.java
@@ -1,75 +1,101 @@
 package com.h8.nh.nhoodengine.core;
 
 
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
 import java.util.Vector;
 
 public abstract class DataFinderAbstractTest<K, D> implements DataFinderRequirements {
 
+    private static final int KEY_VECTOR_SIZE = 3;
+
+    private static final Vector<Integer> KEY_VECTOR_MIN_LIMIT = new Vector<>(Arrays.asList(-1000, -100, -10));
+
+    private static final Vector<Integer> KEY_VECTOR_MAX_LIMIT = new Vector<>(Arrays.asList(10, 100, 1000));
+
+
     private DataFinder<K, D> dataFinder;
 
-    protected abstract void registerData(Vector<K> key, D data);
+    protected abstract Vector<K> mapDataKey(Vector<Integer> key);
+
+    protected abstract D mapDataKeyToData(Vector<K> key);
+
+    protected abstract void registerDataResource(Vector<K> key, D data);
 
     protected abstract DataFinder<K, D> initializeDataFinder();
 
-    @BeforeEach
+    @BeforeAll
     void setUp() {
         dataFinder = initializeDataFinder();
+        for (int i = 0; i < KEY_VECTOR_SIZE; i++) {
+            
+        }
     }
 
-    @Override @Test
+    @Override
+    @Test
     public final void shouldThrowAnExceptionWhenCriteriaIsNull() {
 
     }
 
-    @Override @Test
+    @Override
+    @Test
     public final void shouldThrowAnExceptionWhenCriteriaMetadataVectorIsNull() {
 
     }
 
-    @Override @Test
+    @Override
+    @Test
     public final void shouldThrowAnExceptionWhenCriteriaMetadataVectorSizeDoesNotMatch() {
 
     }
 
-    @Override @Test
+    @Override
+    @Test
     public final void shouldThrowAnExceptionWhenCriteriaLimitIsNegative() {
 
     }
 
-    @Override @Test
+    @Override
+    @Test
     public final void shouldReturnAnEmptyResultListWhenCriteriaLimitZero() {
 
     }
 
-    @Override @Test
+    @Override
+    @Test
     public final void shouldReturnResultListOfLimitSizeWhenCriteriaLimitIsBelowDataSetSize() {
 
     }
 
-    @Override @Test
+    @Override
+    @Test
     public final void shouldReturnWholeResultSetWhenCriteriaLimitHigherThanDataSetSize() {
 
     }
 
-    @Override @Test
+    @Override
+    @Test
     public final void shouldReturnListOfClosestResultForAGivenMetadataVector() {
 
     }
 
-    @Override @Test
+    @Override
+    @Test
     public final void shouldReturnListOfClosestResultForALowestPossibleMetadataVector() {
 
     }
 
-    @Override @Test
+    @Override
+    @Test
     public final void shouldReturnListOfClosestResultForAHighestPossibleMetadataVector() {
 
     }
 
-    @Override @Test
+    @Override
+    @Test
     public final void shouldReturnListOfClosestResultForAAllZeroesMetadataVector() {
 
     }
