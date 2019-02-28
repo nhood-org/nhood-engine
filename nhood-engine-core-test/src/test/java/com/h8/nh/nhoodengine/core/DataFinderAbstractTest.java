69d9e2a1229141073ad968d4383b99f11d4803da
krzysztofreczek <reczek.krzysztof@gmail.com>
2019-02-28 23:35:43 +0100

    NH045 - Implement key generation

--- nhood-engine-core-test/src/test/java/com/h8/nh/nhoodengine/core/DataFinderAbstractTest.java
+++ nhood-engine-core-test/src/test/java/com/h8/nh/nhoodengine/core/DataFinderAbstractTest.java
@@ -1,11 +1,15 @@
 package com.h8.nh.nhoodengine.core;
 
 
-import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Vector;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 public abstract class DataFinderAbstractTest<K, D> implements DataFinderRequirements {
 
@@ -14,7 +18,6 @@ public abstract class DataFinderAbstractTest<K, D> implements DataFinderRequirem
     private static final Vector<Integer> KEY_VECTOR_MIN_LIMIT = new Vector<>(Arrays.asList(-1000, -100, -10));
 
     private static final Vector<Integer> KEY_VECTOR_MAX_LIMIT = new Vector<>(Arrays.asList(10, 100, 1000));
-
 
     private DataFinder<K, D> dataFinder;
 
@@ -26,18 +29,21 @@ public abstract class DataFinderAbstractTest<K, D> implements DataFinderRequirem
 
     protected abstract DataFinder<K, D> initializeDataFinder();
 
-    @BeforeAll
-    void setUp() {
+    @BeforeEach
+    public final void setUp() {
         dataFinder = initializeDataFinder();
-        for (int i = 0; i < KEY_VECTOR_SIZE; i++) {
-            
-        }
+        initializeData();
+    }
+
+    private void initializeData() {
+        generateKeys().map(this::mapDataKey)
+                .forEach(k -> registerDataResource(k, mapDataKeyToData(k)));
     }
 
     @Override
     @Test
     public final void shouldThrowAnExceptionWhenCriteriaIsNull() {
-
+        dataFinder.find(null);
     }
 
     @Override
@@ -98,5 +104,35 @@ public abstract class DataFinderAbstractTest<K, D> implements DataFinderRequirem
     @Test
     public final void shouldReturnListOfClosestResultForAAllZeroesMetadataVector() {
 
+    }
+
+    private Stream<Vector<Integer>> generateKeys() {
+        Stream<Vector<Integer>> keys = generateFirstLevelKeys();
+        for (int i = 1; i < KEY_VECTOR_SIZE; i++) {
+            keys = generateNextLevelKeys(keys, i);
+        }
+        return keys;
+    }
+
+    private Stream<Vector<Integer>> generateFirstLevelKeys() {
+        return IntStream.range(KEY_VECTOR_MIN_LIMIT.get(0), KEY_VECTOR_MAX_LIMIT.get(0))
+                .mapToObj(Vector::new);
+    }
+
+    private Stream<Vector<Integer>> generateNextLevelKeys(Stream<Vector<Integer>> previousLevelKeys, int level) {
+        return previousLevelKeys
+                .flatMap(v -> generateNextLevelKeys(v, level));
+    }
+
+    private Stream<Vector<Integer>> generateNextLevelKeys(Vector<Integer> previousLevelKey, int level) {
+        return IntStream
+                .range(KEY_VECTOR_MIN_LIMIT.get(level), KEY_VECTOR_MAX_LIMIT.get(level))
+                .mapToObj(i -> generateNextLevelKey(previousLevelKey, i));
+    }
+
+    private Vector<Integer> generateNextLevelKey(Vector<Integer> previousLevelKey, Integer nextValue) {
+        List<Integer> nextLevelKey = new ArrayList<>(previousLevelKey);
+        nextLevelKey.add(nextValue);
+        return new Vector<>(nextLevelKey);
     }
 }
