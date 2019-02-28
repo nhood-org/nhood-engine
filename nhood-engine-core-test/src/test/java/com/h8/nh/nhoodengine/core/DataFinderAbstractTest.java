67d333927ef9374ab5fe2f70d4d0ef90e06c4e17
krzysztofreczek <reczek.krzysztof@gmail.com>
2019-02-28 22:02:53 +0100

    NH045 - Finalize requirements

--- nhood-engine-core-test/src/test/java/com/h8/nh/nhoodengine/core/DataFinderAbstractTest.java
+++ nhood-engine-core-test/src/test/java/com/h8/nh/nhoodengine/core/DataFinderAbstractTest.java
@@ -1,17 +1,76 @@
 package com.h8.nh.nhoodengine.core;
 
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-public abstract class DataFinderAbstractTest {
+import java.util.Vector;
 
-    @Test
-    public void shoudlThrowAnExceptionWhenMetadataVectorIsNull() {
+public abstract class DataFinderAbstractTest<K, D> implements DataFinderRequirements {
+
+    private DataFinder<K, D> dataFinder;
+
+    protected abstract void registerData(Vector<K> key, D data);
+
+    protected abstract DataFinder<K, D> initializeDataFinder();
+
+    @BeforeEach
+    void setUp() {
+        dataFinder = initializeDataFinder();
+    }
+
+    @Override @Test
+    public final void shouldThrowAnExceptionWhenCriteriaIsNull() {
 
     }
 
-    @Test
-    public void shoudlThrowAnExceptionWhenMetadataVectorSizeDoesNotMatch() {
+    @Override @Test
+    public final void shouldThrowAnExceptionWhenCriteriaMetadataVectorIsNull() {
+
+    }
+
+    @Override @Test
+    public final void shouldThrowAnExceptionWhenCriteriaMetadataVectorSizeDoesNotMatch() {
+
+    }
+
+    @Override @Test
+    public final void shouldThrowAnExceptionWhenCriteriaLimitIsNegative() {
+
+    }
+
+    @Override @Test
+    public final void shouldReturnAnEmptyResultListWhenCriteriaLimitZero() {
+
+    }
+
+    @Override @Test
+    public final void shouldReturnResultListOfLimitSizeWhenCriteriaLimitIsBelowDataSetSize() {
+
+    }
+
+    @Override @Test
+    public final void shouldReturnWholeResultSetWhenCriteriaLimitHigherThanDataSetSize() {
+
+    }
+
+    @Override @Test
+    public final void shouldReturnListOfClosestResultForAGivenMetadataVector() {
+
+    }
+
+    @Override @Test
+    public final void shouldReturnListOfClosestResultForALowestPossibleMetadataVector() {
+
+    }
+
+    @Override @Test
+    public final void shouldReturnListOfClosestResultForAHighestPossibleMetadataVector() {
+
+    }
+
+    @Override @Test
+    public final void shouldReturnListOfClosestResultForAAllZeroesMetadataVector() {
 
     }
 }
---
+++ nhood-engine-core-test/src/test/java/com/h8/nh/nhoodengine/core/DataFinderRequirements.java
@@ -0,0 +1,17 @@
+package com.h8.nh.nhoodengine.core;
+
+
+public interface DataFinderRequirements {
+
+    void shouldThrowAnExceptionWhenCriteriaIsNull();
+    void shouldThrowAnExceptionWhenCriteriaMetadataVectorIsNull();
+    void shouldThrowAnExceptionWhenCriteriaMetadataVectorSizeDoesNotMatch();
+    void shouldThrowAnExceptionWhenCriteriaLimitIsNegative();
+    void shouldReturnAnEmptyResultListWhenCriteriaLimitZero();
+    void shouldReturnResultListOfLimitSizeWhenCriteriaLimitIsBelowDataSetSize();
+    void shouldReturnWholeResultSetWhenCriteriaLimitHigherThanDataSetSize();
+    void shouldReturnListOfClosestResultForAGivenMetadataVector();
+    void shouldReturnListOfClosestResultForALowestPossibleMetadataVector();
+    void shouldReturnListOfClosestResultForAHighestPossibleMetadataVector();
+    void shouldReturnListOfClosestResultForAAllZeroesMetadataVector();
+}
